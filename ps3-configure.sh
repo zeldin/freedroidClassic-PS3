ccc1505a7ead29ac1d3cc1d11b2aa0d4c3aec9fd
Marcus Comstedt <marcus@mc.pp.se>
2011-01-30 15:49:50 +0100

    Add script to configure for PS3 build.

---
+++ ps3-configure.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+case "$0" in
+  /*) srcdir=`dirname "$0"`;;
+  */*) srcdir=`pwd`/`dirname "$0"`;;
+  *) srcdir=".";;
+esac
+
+LDFLAGS="-B$PSL1GHT/target/lib -B$PS3DEV/host/ppu/lib $PS3DEV/host/ppu/ppu/lib/lv2-psl1ght.o -lpsl1ght -llv2 -L$PS3DEV/ppu/lib"
+CFLAGS="-I$PS3DEV/ppu/include/SDL"
+PATH="$PS3DEV/host/ppu/bin":"$PATH"
+
+export LDFLAGS
+export CFLAGS
+export PATH
+
+"$srcdir"/configure --host=ppu --without-x
