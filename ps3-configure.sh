8e2fc119346304420c4f3e519f0a9bb97b37ca78
Marcus Comstedt <marcus@mc.pp.se>
2011-06-05 10:36:58 +0200

    Updated for new ps3toolchain

--- ps3-configure.sh
+++ ps3-configure.sh
@@ -7,12 +7,10 @@ case "$0" in
 esac
 
 LDFLAGS="-L$PSL1GHT/ppu/lib -L$PS3DEV/portlibs/ppu/lib -lrt -llv2"
-CFLAGS="-I$PSL1GHT/ppu/include/SDL"
-PATH="$PS3DEV/bin":"$PS3DEV/portlibs/ppu/bin":"$PATH"
+PATH="$PS3DEV/bin":"$PS3DEV/ppu/bin":"$PS3DEV/portlibs/ppu/bin":"$PATH"
 
 export LDFLAGS
-export CFLAGS
 export PATH
 
-"$srcdir"/configure --host=ppu --without-x --datarootdir=/dev_hdd0/game/FDROID001/USRDIR
+"$srcdir"/configure --host=powerpc64-ps3-elf --without-x --datarootdir=/dev_hdd0/game/FDROID001/USRDIR
 
