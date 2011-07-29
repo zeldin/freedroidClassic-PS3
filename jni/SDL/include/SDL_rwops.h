d3a91d961b50a9eeac585e48c342b6abfc45ab88
Ryan C. Gordon <icculus@icculus.org>
2011-07-29 16:51:25 -0400

    Allow Android platforms to read from .apk files via the RWOPS interface.
    Fixes Bugzilla #1261.
    
    Thanks to Tim Angus for the patch!

--- jni/SDL/include/SDL_rwops.h
+++ jni/SDL/include/SDL_rwops.h
@@ -83,7 +83,21 @@ typedef struct SDL_RWops
     Uint32 type;
     union
     {
-#ifdef __WIN32__
+#if defined(ANDROID)
+        struct
+        {
+            void *fileName;
+            void *fileNameRef;
+            void *inputStream;
+            void *inputStreamRef;
+            void *skipMethod;
+            void *readableByteChannel;
+            void *readableByteChannelRef;
+            void *readMethod;
+            long position;
+            int size;
+        } androidio;
+#elif defined(__WIN32__)
         struct
         {
             SDL_bool append;
@@ -96,6 +110,7 @@ typedef struct SDL_RWops
             } buffer;
         } windowsio;
 #endif
+
 #ifdef HAVE_STDIO_H
         struct
         {
