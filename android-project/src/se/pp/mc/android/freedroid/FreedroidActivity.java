7d8b7399d1d3cf1fa8fcd38a8aa894fa5416d798
Marcus Comstedt <marcus@mc.pp.se>
2013-07-06 12:04:54 +0200

    ANDROID: Renamed package to avoid conflict

---
+++ android-project/src/se/pp/mc/android/freedroid/FreedroidActivity.java
@@ -0,0 +1,15 @@
+package se.pp.mc.android.freedroid;
+
+import org.libsdl.app.SDLActivity;
+import android.os.Bundle;
+
+public class FreedroidActivity extends SDLActivity
+{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
